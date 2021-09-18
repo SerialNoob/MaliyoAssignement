d45f8b37b9dc331c21ae4ebd5819c7a362853361
serialnoob <emclaurel@gmail.com>
2021-09-18 22:26:30 +0000

    Added Tags for each objects More value for DifficultyUnit and fine tuning Centralised more data in Global.cs and moved out some static calls

--- Assets/Scripts/Data/DifficultyUnit.cs
+++ Assets/Scripts/Data/DifficultyUnit.cs
@@ -7,12 +7,19 @@ public class DifficultyUnit : ScriptableObject
 {
     public float m_CollectibleCount;
     public GameObject[] m_Obstacles;
+    public GameObject[] m_collectible;
     public float m_spawnTime;
+    public float m_intermediateSpawnTime;
 
 
     public GameObject GetObstacle()
     {
         return m_Obstacles[Random.Range(0, m_Obstacles.Length-1)];
+    }
+
+    public GameObject GetCollectible()
+    {
+        return m_collectible[Random.Range(0, m_collectible.Length-1)];
     }
 }
 
--- Assets/Scripts/GLOBALS.cs
+++ Assets/Scripts/GLOBALS.cs
@@ -2,11 +2,19 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Globals 
+public class Globals
 {
     public static float SPEED = 4;
     public static bool USE_SEED_GENERATION = true;
     public static string SEED_STRING = "123456";
+    public static int MAX_FUEL = 5;
+    public static int MAX_LIFE = 2;
+    public static float Fuel_CONSUMPTIONTIME = 5;
+
+    static Globals()
+    {
+
+    }
 
 
     public enum Level_DIFFICULTY
